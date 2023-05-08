f8c8e9633ab27d4bbeb3171b7703ea6b51fc906e
tobato <you@google.com>
2023-05-08 14:36:17 -0300

    arreglado(otra vez(otra vez))

--- tp redes giercowski gonzalez/JWT/key.ts
+++ tp redes giercowski gonzalez/JWT/key.ts
@@ -2,21 +2,21 @@ import { tokenToString } from "typescript";
 
 export const SECRET_KEY = "catALEJO";
 const jwt = require("jsonwebtoken");
-export function verifyToken(req: any, res: any, next: any) {
-    const token = req.headers.authorization;
+export function verifyToken(_req: any, _res: any, next: any) {
+    const token = _req.headers.authorization;
     console.log(token);
     if (!token) {
-        return res
+        return _res
             .status(401)
             .send({ message: "Unauthorized: No token provided." });
     }
 
     try {
         const decoded = jwt.verify(token, SECRET_KEY);
-        req.usuario = decoded;
+        _req.usuario = decoded;
         console.log("verificación exitosa :D");
         next();
     } catch (err) {
-        return res.status(401).send({ message: "Unauthorized: Invalid token." });
+        return _res.status(401).send({ message: "Unauthorized: Invalid token." });
     }
 }
